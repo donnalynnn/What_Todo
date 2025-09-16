9d88c290112dc26ceed5e4f7cb952596bf2665d4
donnalynnn <donnalyn.dev@gmail.com>
2025-09-16 14:05:49 +0800

    fix azure wwwroot case

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
+
+Route::get('/tasks', [TaskController::class, 'index']);
+Route::post('/tasks', [TaskController::class, 'store']);
+Route::put('/tasks/{task}', [TaskController::class, 'update']);
+Route::delete('/tasks/{task}', [TaskController::class, 'destroy']);
+Route::get('/healthcheck', function () {
+    return response()->json(['status' => 'ok']);
+});
