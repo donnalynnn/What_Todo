94619383e20812a7fd7e91779a879c0358873890
donnalynnn <donnalyn.dev@gmail.com>
2025-09-16 11:28:53 +0800

    url update

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import type {AxiosInstance} from "axios";
 
 const api: AxiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: "https://whatodo.azurewebsites.net/api"
 });
 
 export default api;
